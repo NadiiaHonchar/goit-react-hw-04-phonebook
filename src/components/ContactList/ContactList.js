8b8f40e189d41c8c0e947c4026711a6a93bc91d2
NadiiaHonchar <17nmg82@gmail.com>
2022-01-28 19:33:14 +0200

    settings

---
+++ src/components/ContactList/ContactList.js
@@ -0,0 +1,29 @@
+import React from "react";
+import PropTypes from "prop-types";
+import style from "./ContactList.module.css";
+import ElementContacts from "../ElementContacts";
+
+const ContactList = ({ contacts, onDelContact }) => {
+  return (
+    <ul className={style.ulStyle}>
+      {contacts.map((contact) => (
+        <li key={contact.id}>
+          <ElementContacts contacts={contact} onDelContact={onDelContact} />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ),
+  onDelContact: PropTypes.func.isRequired,
+};
+
+export default ContactList;
